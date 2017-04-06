4f2bf838f8da81598ce4c1deaf26174aa7a24c03
Michael Mehok <michael.mehok@bancvue.com>
2017-04-06 16:11:50 -0500

    making stuff better

--- envadapter_test.go
+++ envadapter_test.go
@@ -81,3 +81,25 @@ func TestEnvAdapter_CanReadFromEnv_OnlyComma(t *testing.T) {
 
 	os.Unsetenv("SVC_TEST_SVC_ONE_ADDR")
 }
+
+func TestEnvAdapter_CanReadFromEnv_MultipleSvcEnvs(t *testing.T) {
+	// given
+	os.Setenv("SVC_TEST_SVC_ONE_ADDR", "test567:8080")
+	os.Setenv("SVC_TEST_SVC_TWO_ADDR", "test890:9090")
+
+	// when
+	ea := NewEnvAdapter()
+
+	svc1, _ := ea.GetService("test-svc-one")
+	svc2, _ := ea.GetService("test-svc-two")
+
+	// then
+	assert.Equal(t, "test-svc-one", svc1.Name)
+	assert.Equal(t, []Address{Address{Host: "test567", Port: "8080"}}, svc1.Addresses)
+
+	assert.Equal(t, "test-svc-two", svc2.Name)
+	assert.Equal(t, []Address{Address{Host: "test890", Port: "9090"}}, svc2.Addresses)
+
+	os.Unsetenv("SVC_TEST_SVC_ONE_ADDR")
+	os.Unsetenv("SVC_TEST_SVC_TWO_ADDR")
+}
